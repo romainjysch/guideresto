24c0db90eb28e291e7c8eccb9540570f5fedb431
romainjysch <rom.jys@outlook.com>
2023-04-01 14:56:41 +0200

    refactor(guide-resto) : Add Connection attribute to the DBTransaction class

--- src/main/java/ch/hearc/ig/guideresto/persistence/DBTransaction.java
+++ src/main/java/ch/hearc/ig/guideresto/persistence/DBTransaction.java
@@ -12,8 +12,9 @@ public class DBTransaction {
     private static final String DBURL = "jdbc:oracle:thin:@db.ig.he-arc.ch:1521:ens";
     private static final String DBUSER = "romain_jysch";
     private static final String DBPWD = "romain_jysch";
+    private final Connection connection;
 
-    public Connection openConnection() {
+    private Connection openConnection() {
         try {
             Connection cnn = DriverManager.getConnection(DBURL, DBUSER, DBPWD);
             cnn.setAutoCommit(false);
@@ -24,10 +25,11 @@ public class DBTransaction {
         }
     }
 
-    public DBTransaction() {}
+    public DBTransaction() {
+        this.connection = openConnection();
+    }
 
     public <T> T functionTransaction(Function<Connection, T> function) {
-        Connection connection = openConnection();
         try {
             T result = function.apply(connection);
             connection.commit();
